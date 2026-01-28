6df9246c3825c3dd124b3e294d8eb5154d2ef145
FakhrunnisaMohammadi <fakhrunnisa.mohammadi@gmail.com>
2026-01-28 23:52:47 +0600

    Add files via upload

--- GradeBook.java
+++ GradeBook.java
@@ -1,59 +1,97 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class GradeBook {
+    // Inner Class:StudentCourseKey
+    class StudentCourseKey implements Comparable<StudentCourseKey> {
+        String studentId;
+        String courseCode;
+
+        public StudentCourseKey(String studentId, String courseCode) {
+            this.studentId = studentId;
+            this.courseCode = courseCode;
+        }
+
+        // Compare first by studentId and after that by courseCode
+        @Override
+        public int compareTo(StudentCourseKey other) {
+            int idCompare = this.studentId.compareTo(other.studentId);
+
+            // if studentIds are equal then compare course codes
+            if (idCompare == 0) {
+                return this.courseCode.compareTo(other.courseCode);
+            }
+            return idCompare;
+        }
+    }
+
     TreeMap<StudentCourseKey, Double> gradeBook = new TreeMap<>();
 
-    // assing a grade to a student
+    // assign the Grade
     public void assignGrade(String studentId, String courseCode, double grade) {
         StudentCourseKey key = new StudentCourseKey(studentId, courseCode);
         gradeBook.put(key, grade);
     }
 
-    // get grade for a specific student in a specific course
+    // get the Grade
     public double getGrade(String studentId, String courseCode) {
-        Double d = gradeBook.get(new StudentCourseKey(studentId, courseCode));
-        return d == null ? -1 : d;
+        StudentCourseKey key = new StudentCourseKey(studentId, courseCode);
+
+        if (gradeBook.containsKey(key)) {
+            return gradeBook.get(key);
+        }
+        return -1; // grade not found
     }
 
-    // get the student gpa using student id
+    // get Student GPA
     public double getStudentGPA(String studentId) {
         double sum = 0;
         int count = 0;
-        for (Map.Entry<StudentCourseKey, Double> e : gradeBook.entrySet()) {
-            if (e.getKey().getStudentId().equals(studentId)) {
-                sum += e.getValue();
+
+        for (Map.Entry<StudentCourseKey, Double> entry : gradeBook.entrySet()) {
+            if (entry.getKey().studentId.equals(studentId)) {
+                sum += entry.getValue();
                 count++;
             }
         }
-        return count == 0 ? -1 : (sum / count); // -1 means no records for student
+
+        if (count == 0)
+            return -1; // no records for student
+        return sum / count;
     }
 
-    public List<Map.Entry<StudentCourseKey, Double>> getAllGradesForStudent(String studentId) {
-        List<Map.Entry<StudentCourseKey, Double>> result = new ArrayList<>();
-        for (Map.Entry<StudentCourseKey, Double> e : gradeBook.entrySet()) {
-            if (e.getKey().getStudentId().equals(studentId))
-                result.add(e);
+    // get all grades for a student
+    public void getAllGradesForStudent(String studentId) {
+        System.out.println("Grades for Student: " + studentId);
+
+        for (Map.Entry<StudentCourseKey, Double> entry : gradeBook.entrySet()) {
+            if (entry.getKey().studentId.equals(studentId)) {
+                System.out.println("Course: " + entry.getKey().courseCode +
+                        "Grade: " + entry.getValue());
+            }
         }
-        return result;
     }
 
-    // get the highest grad between students
+    // get highest grade
     public double getHighestGrade() {
         double highest = -1;
-        for (Double grade : gradeBook.values()) {
-            if (grade > highest)
+
+        for (double grade : gradeBook.values()) {
+            if (grade > highest) {
                 highest = grade;
+            }
         }
         return highest;
     }
 
-    // display grades sorted way
+    // display grades sorted
     public void displayGradesSorted() {
+        System.out.println("=== Sorted Grades ===");
+
         for (Map.Entry<StudentCourseKey, Double> entry : gradeBook.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+            System.out.println("Student: " + entry.getKey().studentId +
+                    ", Course: " + entry.getKey().courseCode +
+                    "Grade: " + entry.getValue());
         }
     }
 }
